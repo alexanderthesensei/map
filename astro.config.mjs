9ebf81e7044c3071c10ba8aec6acab7bdcc6519a
Alexander <alexanderthestudent@yandex.ru>
2024-10-06 09:26:49 +0000

    fix: baseUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import react from '@astrojs/react';
 export default defineConfig({
   integrations: [react()],
   site: 'https://alexanderthesensei.github.io',
-  base: 'history'
+  base: 'history_display'
 });
