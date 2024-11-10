26977e60f96df714d9a3bcf3eef623561ed61ce6
Alexander <alexanderthestudent@yandex.ru>
2024-11-10 07:39:55 +0000

    Revert "deploy: add netlify"
    This reverts commit f5be80f88f64fae667905020b22b9f453fb0d2eb.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: 'https://alexanderthesensei.github.io',
-  base: 'map',
-  output: 'server',
-  adapter: netlify()
+  base: 'map'
 });
